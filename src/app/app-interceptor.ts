c2a54f1aaf387b45689f7591bc0e2bd9d5a413ae
nina <ninagbs@abv.bg>
2023-07-25 16:57:01 +0300

    added error component

--- src/app/app-interceptor.ts
+++ src/app/app-interceptor.ts
@@ -1,24 +1,32 @@
-import {  HTTP_INTERCEPTORS, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { HTTP_INTERCEPTORS, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { Injectable, Provider } from "@angular/core";
-import { Observable } from "rxjs";
+import { Router } from "@angular/router";
+import { Observable, catchError } from "rxjs";
 import { environment } from "src/environments/environment";
-const {apiUrl} =environment;
+import { ErrorService } from "./core/error/error.service";
+const { apiUrl } = environment;
 
 @Injectable()
-export class AppInterceptor implements  HttpInterceptor{
-
-    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-      if(req.url.startsWith('/api')){
-        req = req.clone({
-            url: req.url.replace('/api', apiUrl),
-            withCredentials: true,
-        })
-      }
-      return next.handle(req)
+export class AppInterceptor implements HttpInterceptor {
+  constructor(private router: Router, private errorService: ErrorService) { }
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (req.url.startsWith('/api')) {
+      req = req.clone({
+        url: req.url.replace('/api', apiUrl),
+        withCredentials: true,
+      })
     }
+    return next.handle(req).pipe(
+      catchError((err) => {
+        this.errorService.setError(err)
+        this.router.navigate(['/error'])
+        return [err];
+      })
+    )
+  }
 }
 export const appInterceptorProvider: Provider = {
-    multi: true,
-    useClass: AppInterceptor,
-    provide: HTTP_INTERCEPTORS
+  multi: true,
+  useClass: AppInterceptor,
+  provide: HTTP_INTERCEPTORS
 }
