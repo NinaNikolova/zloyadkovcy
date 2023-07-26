d9fd4aa48b8769904330ee7357833c7f8807ff16
nina <ninagbs@abv.bg>
2023-07-26 18:05:21 +0300

    removint pipe times

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,15 +4,14 @@ import { LoaderComponent } from './loader/loader.component';
 import { RouterModule } from '@angular/router';
 import { SlicePipe } from './pipes/slice.pipe';
 import { UppercasePipe } from './pipes/uppercase.pipe';
-import { TimesPipe } from './pipes/times.pipe';
 
 
 
 @NgModule({
-  declarations: [LoaderComponent, SlicePipe, UppercasePipe, TimesPipe],
+  declarations: [LoaderComponent, SlicePipe, UppercasePipe],
   imports: [
     CommonModule, RouterModule
   ],
-  exports:[LoaderComponent, SlicePipe, UppercasePipe, TimesPipe]
+  exports:[LoaderComponent, SlicePipe, UppercasePipe]
 })
 export class SharedModule { }
