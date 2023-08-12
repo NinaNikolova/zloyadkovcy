d822514a8ac0b84397c19edc4d372277ba822e71
nina <ninagbs@abv.bg>
2023-08-12 12:06:12 +0300

    some changes in pipes

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { SlicePipe } from './pipes/slice.pipe';
-import { UppercasePipe } from './pipes/uppercase.pipe';
 import { ConfirmationDialogComponent } from './confirmation-dialog/confirmation-dialog.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
@@ -15,13 +14,13 @@ import { Search2Pipe } from './pipes/search2.pipe';
 
 
 @NgModule({
-  declarations: [SlicePipe, UppercasePipe, ConfirmationDialogComponent, SearchPipe, ListPipe, Search2Pipe],
+  declarations: [SlicePipe, ConfirmationDialogComponent, SearchPipe, ListPipe, Search2Pipe],
   imports: [
     CommonModule,
     MatButtonModule,
     MatDialogModule,
     RouterModule
   ],
-  exports:[SlicePipe, UppercasePipe, SearchPipe, ListPipe, Search2Pipe]
+  exports:[SlicePipe, SearchPipe, ListPipe, Search2Pipe]
 })
 export class SharedModule { }
